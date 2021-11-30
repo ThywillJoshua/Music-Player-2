d45d1708151828f9ba88bfd74ef548dd96feb000
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2021-11-30 20:11:54 +0100

    add git link

---
+++ data.js
@@ -0,0 +1,90 @@
+//songs
+export default function chillHop() {
+  return [
+    {
+      name: "Spunk",
+      cover:
+        "https://i1.wp.com/xclusiveloaded.com/wp-content/uploads/2021/08/Major-League-DJz-ft-Wurld-LuuDadeejay-Stamina.jpg?fit=500%2C500&ssl=1",
+      artist: "Wurld",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2021/08/WurlD_-_Spunk.mp3",
+      color: ["#86DEC1", "#1F4669"],
+      id: 0,
+    },
+
+    {
+      name: "Pay",
+      cover:
+        "https://i1.wp.com/xclusiveloaded.com/wp-content/uploads/2021/08/Oxlade-%E2%80%93-Eclipse0AEP.jpg?fit=1000%2C1000&ssl=1",
+      artist: "Oxlade",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2021/08/Oxlade_-_Pay_Me.mp3",
+      color: ["#E9B629", "#273E6E"],
+      id: 1,
+    },
+    {
+      name: "Pull Up",
+      cover:
+        "https://i1.wp.com/xclusiveloaded.com/wp-content/uploads/2021/05/Gabzy-%E2%80%93-Pull-Up.jpg?fit=1024%2C1024&ssl=1",
+      artist: "Gabzy",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2021/05/Gabzy_-_Pull_Up.mp3",
+      color: ["#E42538", "#1B1F85"],
+      id: 2,
+    },
+    {
+      name: "Corny",
+      cover:
+        "https://i2.wp.com/xclusiveloaded.com/wp-content/uploads/2021/05/Rema-Peace-Of-Mind-1.jpeg?fit=600%2C600&ssl=1",
+      artist: "Rema",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2019/07/Rema_-_Corny.mp3",
+      color: ["#FEEC8F", "#E53D21"],
+      id: 3,
+    },
+
+    {
+      name: "Puttin",
+      cover:
+        "https://i2.wp.com/xclusiveloaded.com/wp-content/uploads/2020/10/Tekno-Puttin.jpg?fit=600%2C600&ssl=1",
+      artist: "Tekno",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2020/10/Tekno_-_Puttin.mp3",
+      color: ["#ECD8D2", "#474A4B"],
+      id: 4,
+    },
+
+    {
+      name: "Infinity",
+      cover:
+        "https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2020/10/olamide-Carpe-Diem-scaled-1.jpg?fit=768%2C768&ssl=1",
+      artist: "Olamide",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2020/10/Olamide_ft_Omah_Lay_-_Infinity.mp3",
+      color: ["#FABE2B", "#7B110F"],
+      id: 5,
+    },
+
+    {
+      name: "234",
+      cover:
+        "https://i0.wp.com/xclusiveloaded.com/wp-content/uploads/2021/10/MINZ-234.jpg?fit=500%2C500&ssl=1",
+      artist: "Minz",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2021/10/Minz_-_234.mp3",
+      color: ["#A5BDA9", "#324A35"],
+      id: 6,
+    },
+
+    {
+      name: "Juba (Bow)",
+      cover:
+        "https://i2.wp.com/xclusiveloaded.com/wp-content/uploads/2021/04/Peruzzi-Rum-Boogie.jpeg?fit=500%2C500&ssl=1",
+      artist: "Peruzzi",
+      audio:
+        "https://www.xclusiveloaded.com/wp-content/uploads/2021/04/Peruzzi_-_Juba_Bow_.mp3",
+      color: ["#B08C32", "#40160C"],
+      id: 7,
+    },
+  ];
+}
